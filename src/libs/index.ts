5388435e2de628b614373fb79dc6dcf1f4b4e291
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-10-06 17:52:08 +0200

    Made various changes :
    - Added index.css import to index.ts
    - Added detailed package info in package.json
    - Configured vite.config.ts & package.json for build

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,3 +1,4 @@
+import "../index.css";
 import Modal from "./Modal";
 
 export default Modal;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/libs/index.ts"),
+      name: "vz-react-modal",
+      fileName: "vz-react-modal"
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React"
+        }
+      }
+    }
+  }
+});
