12b38aff09bc5aec7905ab611d9dffdd89d37a20
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-10-07 12:13:24 +0200

    Made various changes :
    - Edited config in vite.config.ts to fix TS error
    - Changed version to 1.0.2 in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,27 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "./src/libs/index.ts"),
       name: "vz-react-modal",
-      fileName: "vz-react-modal"
+      formats: ["es", "umd"],
+      fileName: (format) => `vz-react-modal.${format}.js`
     },
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
         globals: {
-          react: "React"
+          react: "React",
+          "react-dom": "ReactDOM"
         }
       }
     }
-  },
-  plugins: [react(), dts({ rollupTypes: true })]
+  }
 });
